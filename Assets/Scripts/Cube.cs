2655bd99e09d34b3000224f0aaa1073b28682e4c
dv.barashkov <dv.barashkov@tensor.ru>
2025-10-17 10:22:44 +0300

    init

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class Cube : MonoBehaviour
+{
+    private Renderer _renderer;
+    private bool _isCollidedWithPlain;
+    private int _minDestroyDelay = 2;
+    private int _maxDestroyDelay = 4;
+
+    public event UnityAction<GameObject> DestroyCube;
+
+    private void OnEnable()
+    {
+        _renderer = GetComponent<Renderer>();
+        _isCollidedWithPlain = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (_isCollidedWithPlain == false && collision.gameObject.tag == "Plain")
+        {
+            _isCollidedWithPlain = true;
+            SetColor();
+
+            Invoke(nameof(InvokeDestroyEvent), Random.Range(_minDestroyDelay, _maxDestroyDelay + 1));
+        }
+    }
+    private void SetColor()
+    {
+        Color randomColor = Random.ColorHSV();
+
+        _renderer.material.color = randomColor;
+    }
+
+    private void InvokeDestroyEvent()
+    {
+        DestroyCube?.Invoke(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _prefab;
+    [SerializeField] private float _repeatRate = 1f;
+    [SerializeField] private int _poolCapacity = 5;
+    [SerializeField] private int _poolMaxSize = 5;
+
+    private ObjectPool<GameObject> _pool;
+    private int _minPositionCoordinate = -5;
+    private int _maxPositionCoordinate = 5;
+    private int _spawnHeight;
+
+    private void Awake()
+    {
+        _pool = new ObjectPool<GameObject>(
+        createFunc: () => Instantiate(_prefab),
+        actionOnGet: (obj) => ActionOnGet(obj),
+        actionOnRelease: (obj) => ActionOnRelease(obj),
+        actionOnDestroy: (obj) => ActionOnDestroy(obj),
+        collectionCheck: true,
+        defaultCapacity: _poolCapacity,
+        maxSize: _poolMaxSize);
+    }
+
+    private void ActionOnGet(GameObject obj)
+    {
+        obj.transform.position = new Vector3(Random.Range(_minPositionCoordinate, _maxPositionCoordinate), 
+                                            _spawnHeight, 
+                                            Random.Range(_minPositionCoordinate, _maxPositionCoordinate));
+        obj.transform.rotation = Quaternion.Euler(0, 0, 0);
+        obj.GetComponent<Renderer>().material.color = Color.white;
+        obj.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        obj.SetActive(true);
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(GetCube), 0.0f, _repeatRate);
+    }
+
+    private void GetCube()
+    {
+        GameObject obj = _pool.Get();
+        obj.GetComponent<Cube>().DestroyCube += ReleaseObject;
+    }
+
+    private void ReleaseObject(GameObject obj)
+    {        
+        _pool.Release(obj);
+    }
+    private void ActionOnRelease(GameObject obj)
+    {
+        obj.SetActive(false);
+    }
+
+    private void ActionOnDestroy(GameObject obj)
+    {
+        obj.GetComponent<Cube>().DestroyCube -= ReleaseObject;
+        Destroy(obj);
+    }
+}
